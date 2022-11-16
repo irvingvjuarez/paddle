2c67ee5bd14a3cf1747aa34450dca67144ab5c0e
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-16 17:58:31 -0600

    Introduction to canvas

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const canvas = document.getElementById("game-screen")
+const ctx = canvas.getContext("2d")
+
+ctx.clearRect(0, 0, 800, 600)
+
+ctx.fillStyle = "#f00"
+ctx.fillRect(5, 5, 50, 50);
+
+ctx.fillStyle = "#00f"
+ctx.fillRect(70, 70, 15, 15)
