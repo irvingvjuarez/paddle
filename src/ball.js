4ce9a1ec9cd944105c1d5f2f76e7b215042048de
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-24 21:08:49 -0600

    Constraints set to the ball movement

--- src/ball.js
+++ src/ball.js
@@ -1,3 +1,4 @@
+import { GAME_HEIGHT, GAME_WIDTH } from "./globals.js"
 import { clearCanvas } from "./services/clearCanvas.js"
 
 export class Ball {
@@ -9,6 +10,20 @@ export class Ball {
 	}
 
 	draw(ctx) {
+		let { x: xAxis, y: yAxis } = this.position
+		let { x: xSpeed, y: ySpeed } = this.speed
+
+		const nextXPosition = xAxis += xSpeed
+		const nextYPosition = yAxis += ySpeed
+
+		if(nextXPosition <= 0 || nextXPosition >= GAME_WIDTH) {
+			this.speed.x = this.speed.x * - 1
+		}
+
+		if(nextYPosition <= 0 || nextYPosition >= GAME_HEIGHT) {
+			this.speed.y = this.speed.y * - 1
+		}
+
 		this.position.x += this.speed.x
 		this.position.y += this.speed.y
 
