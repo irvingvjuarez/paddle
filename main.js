16d180c3ea6782ae7d217e52a0af63638240a9a6
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-24 20:10:30 -0600

    Using request animation frame method

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { Ball } from "./src/ball.js"
 import { movementListener } from "./src/listeners/movementListener.js"
 import { Paddle } from "./src/paddle.js"
 
+let initialTime = 0;
+
 const ballElement = document.getElementById("ball-img")
 
 const canvas = document.getElementById("game-screen")
@@ -10,9 +12,16 @@ const ctx = canvas.getContext("2d")
 const paddle = new Paddle()
 const ball = new Ball(ballElement)
 
-paddle.draw(ctx)
-ball.start(ctx)
+// ball.start(ctx)
 
 movementListener()
+
+function gameLoop(time) {
+	paddle.draw(ctx)
+
+	requestAnimationFrame(gameLoop)
+}
+
+requestAnimationFrame(gameLoop)
 
 export { ctx, paddle }
