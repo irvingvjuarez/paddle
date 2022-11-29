e9fbb29d480851aa19c6062faad0d04640171872
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-29 12:22:59 -0600

    Brick image added

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ import { Ball } from "./src/ball.js"
 import { Game } from "./src/game.js";
 import { movementListener } from "./src/listeners/movementListener.js"
 import { Paddle } from "./src/paddle.js"
-import { clearCanvas } from "./src/services/clearCanvas.js";
-
-let initialTime = 0;
 
 const ballElement = document.getElementById("ball-img")
 
@@ -14,9 +11,9 @@ const ctx = canvas.getContext("2d")
 const paddle = new Paddle()
 const ball = new Ball(ballElement)
 
-movementListener()
-
 const game = new Game()
 game.start()
+
+movementListener()
 
 export { ctx, paddle, ball }
