e4533a7a33a6391c6767a3780c181ae3b85b4ca1
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-30 17:11:08 -0600

    Making the bricks changing the ball on intersection

--- src/brick.js
+++ src/brick.js
@@ -1,3 +1,5 @@
+import { ball } from "../main.js"
+
 export class Brick {
 	constructor(imgElement, coords =  { x: 0, y: 0, w: 100, h: 50 }) {
 		this.img = imgElement
@@ -5,6 +7,16 @@ export class Brick {
 	}
 
 	draw(ctx) {
+		const { x: ballX, y: ballY } = ball.position
+		const ballSize = ball.size
+		const intersectionX = ballX >= this.coords.x && ballX + ballSize <= this.coords.x + this.coords.w
+		const intersectionY = ballY <= this.coords.y + this.coords.h
+		const brickIntersection = intersectionX && intersectionY
+
+		if (brickIntersection) {
+			ball.changeDirectionInY()
+		}
+
 		ctx.drawImage(
 			this.img,
 			this.coords.x,
