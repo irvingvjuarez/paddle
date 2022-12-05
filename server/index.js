11b584e9942699c5f5565e11879eca6bb89cd53e
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-05 17:59:10 -0600

    hello world on express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ import express from "express"
 const PORT_NUMBER = 3000;
 
 const app = express()
+
+// GET
+app.get("/", (req, res) => {
+	res.send("Hello from express.js")
+})
+
 app.listen(PORT_NUMBER, () => {
 	console.log(`Server listening on http://localhost:${PORT_NUMBER}`)
 })
