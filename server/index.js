8ed6c2a04ceba183d94e8f3e442cd4e87602502c
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-05 17:16:04 -0600

    minimum viable server with express.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+import express from "express"
+const PORT_NUMBER = 3000;
+
+const app = express()
+app.listen(PORT_NUMBER, () => {
+	console.log(`Server listening on http://localhost:${PORT_NUMBER}`)
+})
