3ef8d0ca46475c412cc785205b7d95941b64da66
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-06 15:04:22 -0600

    Knowing when the input is checked

--- js/multi-player.js
+++ js/multi-player.js
@@ -1,1 +1,12 @@
-console.log("Hello world from multi player page")
+const newGameInput = document.getElementById("new-game")
+const codeGameInput = document.getElementById("code-game")
+
+const multiPlayerInputs = [newGameInput, codeGameInput]
+
+multiPlayerInputs.forEach(input => {
+	input.addEventListener("change", (evt) => {
+		if (evt.target.checked) {
+			console.log("is checked")
+		}
+	})
+})
