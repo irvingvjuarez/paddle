9a55d6387918aea301a4170244375cbff1755fc4
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-07 16:38:42 -0600

    action on submit multi player page

--- js/multi-player.js
+++ js/multi-player.js
@@ -1,6 +1,7 @@
 const newGameInput = document.getElementById("new-game")
 const codeGameInput = document.getElementById("code-game")
 const nextbutton = document.querySelector(".multi-player-form-submit")
+const form = document.querySelector(".multi-player-form")
 
 const multiPlayerInputs = [newGameInput, codeGameInput]
 let isNextButtonAvailable = false
@@ -15,4 +16,11 @@ function checkButtonAvailability() {
 
 multiPlayerInputs.forEach(input => {
 	input.addEventListener("change", checkButtonAvailability)
+})
+
+form.addEventListener("submit", (evt) => {
+	evt.preventDefault()
+
+	const chosenValue = multiPlayerInputs.find(input => input.checked)
+	console.log(chosenValue.id)
 })
