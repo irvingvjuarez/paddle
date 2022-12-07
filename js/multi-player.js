210f5d8975e0af16a888293cde87322c7330d9b0
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-07 16:22:28 -0600

    multi player page ready to go on

--- js/multi-player.js
+++ js/multi-player.js
@@ -1,12 +1,18 @@
 const newGameInput = document.getElementById("new-game")
 const codeGameInput = document.getElementById("code-game")
+const nextbutton = document.querySelector(".multi-player-form-submit")
 
 const multiPlayerInputs = [newGameInput, codeGameInput]
+let isNextButtonAvailable = false
+
+function checkButtonAvailability() {
+	isNextButtonAvailable = multiPlayerInputs.some(input => input.checked)
+
+	if (isNextButtonAvailable) {
+		nextbutton.removeAttribute("disabled")
+	}
+}
 
 multiPlayerInputs.forEach(input => {
-	input.addEventListener("change", (evt) => {
-		if (evt.target.checked) {
-			console.log("is checked")
-		}
-	})
+	input.addEventListener("change", checkButtonAvailability)
 })
