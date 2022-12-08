7fafe0947e520f61e941d0a495970052c188824c
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-07 19:11:11 -0600

    new game variant html done

--- js/services/newGameVariant.js
+++ js/services/newGameVariant.js
@@ -1,3 +1,20 @@
 export const newGameVariant = () => {
-	console.log("New Game variant")
+	const newGameCode = Math.ceil(Math.random() * 60000);
+	const mainContainer = document.querySelector("main")
+
+	mainContainer.innerHTML = `
+		<section class="new-game-container">
+			<h2>Share the following code to start playing</h2>
+
+			<span class="game-code">${newGameCode}</span>
+
+			<section class="game-room">
+				<h3>Current room members</h3>
+
+				<article>
+					<span>You</span>
+				</article>
+			</section>
+		</section>
+	`
 }
